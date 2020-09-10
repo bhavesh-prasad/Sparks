166d895cd0cf7f9f2562ca8488d62c130a794b0a
= <=>
2020-09-10 22:42:35 +0530

    Inital Design

---
+++ App.py
@@ -0,0 +1,37 @@
+from flask import Flask,flash,render_template,request,redirect,url_for
+import sqlite3 as sql
+app= Flask(__name__)
+import DBHandler as db
+import os
+from flask_toastr import Toastr
+toastr = Toastr(app)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+@app.route('/')
+@app.route('/home')
+def Home():
+   con=sql.connect('Database.db')
+   cur=con.cursor()
+   det=cur.execute('Select * from Users')
+   return render_template('Home.html',users=det)
+
+@app.route('/users',methods=['GET','POST'])
+def users():
+   if request.method== 'POST':
+      con=sql.connect('Database.db')
+      cur=con.cursor()
+      reciever_id = request.form.get("Reciever_id")
+      credit = request.form.get("Credit")
+      if reciever_id==request.args.get('uid'):
+         flash("You cannot send money to yourself")
+         return render_template('TransferCredit.html')      
+      cur.execute("update users set credit_number =credit_number +(?) where uid=(?)",(credit,reciever_id))
+      cur.execute("update users set credit_number =credit_number -(?) where uid=(?)",(credit,request.args.get('uid')))
+      cur.execute('insert into logs(sender,reciever,credit_number) values(?,?,?) ',(request.args.get('uid'),reciever_id,credit))
+      con.commit()
+      return redirect(url_for('Home'))
+   return render_template('TransferCredit.html')
+
+
+if __name__ == "__main__":
+   os.system('python3 DBHandler.py')
+   app.run(debug=True)
---
+++ DBHandler.py
@@ -0,0 +1,22 @@
+
+import sqlite3 as sql
+con=sql.connect('Database.db')
+cur=con.cursor()
+cur.execute('Create table if not exists USERS ( uid integer primary key AUTOINCREMENT, fname text , lname text , credit_number number ) ')
+cur.execute('Create table if not exists LOGS (  TRANSACTION_ID integer primary key AUTOINCREMENT, Sender number  , Reciever number ,credit_number number ,foreign key(Sender) references Users(uid),foreign key(Reciever) references Users(uid))')
+if cur.execute('Select count(*) from Users').fetchall()[0][0]==0:    
+    cur.execute('Insert into users(fname,lname,credit_number) values("Bhavesh" , "Prasad", 1000 )') 
+    cur.execute('Insert into users(fname,lname,credit_number) values("Aditya" , "Akash", 500 )') 
+    cur.execute('Insert into users(fname,lname,credit_number) values("Alex" , "Roy", 5200 )') 
+    cur.execute('Insert into users(fname,lname,credit_number) values("Frank" , "Borne", 1500 )') 
+    cur.execute('Insert into users(fname,lname,credit_number) values("Summer" , "Stay", 5300 )') 
+    cur.execute('Insert into users(fname,lname,credit_number) values("John" , "Doe", 7500 )') 
+    cur.execute('Insert into users(fname,lname,credit_number) values("Alex" , "Teidmann", 4500 )') 
+    cur.execute('Insert into users(fname,lname,credit_number) values("Regina" , "Filangae",2500 )') 
+    cur.execute('Insert into users(fname,lname,credit_number) values("Joey" , "Tribi", 3500 )') 
+    cur.execute('Insert into users(fname,lname,credit_number) values("Ash" , "Bourne", 2500 )') 
+    con.commit()
+
+# def insertTransaction(from_user,to_user,amount):
+#     cur.execute('Insert into logs(sender,reciever,credit) values(?,?,?)',(from_user,to_user,amount))
+#     con.commit 
